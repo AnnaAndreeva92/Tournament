7c6b68d177fe8c9cee3e4a533e35aab3b8b34e25
AnnaAndreeva92 <ann-stesha@yandex.ru>
2022-08-13 11:06:47 +0300

    tournament

---
+++ src/main/java/ru/netology/Player.java
@@ -0,0 +1,13 @@
+package ru.netology;
+
+public class Player {
+    public int id;
+    public String name;
+    public int strength;
+
+    public Player(int id, String name, int strength) {
+        this.id = id;
+        this.name = name;
+        this.strength = strength;
+    }
+}
